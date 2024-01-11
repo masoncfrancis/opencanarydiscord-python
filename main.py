cb2c97910f0954bdea84660701b3cf9a887bf0b8
Mason <masoncfrancis@gmail.com>
2024-01-11 16:55:16 -0700

    updated file formatting

--- main.py
+++ main.py
@@ -61,5 +61,5 @@ def handler():
     return jsonify({"statusCode": response.status_code, "body": response.text})
 
 if __name__ == '__main__':
-
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    
